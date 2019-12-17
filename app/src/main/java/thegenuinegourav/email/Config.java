5f10d39d560a02053bdcd273a0cd757174b05255
Tusar0003 <faisalahmedtusar@gmail.com>
2019-12-17 16:02:21 +0600

    Initial Commit

---
+++ app/src/main/java/thegenuinegourav/email/Config.java
@@ -0,0 +1,6 @@
+package thegenuinegourav.email;
+
+public class Config {
+    public static final String EMAIL ="faisalahmedtusar@gmail.com"; //your-gmail-username
+    public static final String PASSWORD =""; //your-gmail-password
+}
